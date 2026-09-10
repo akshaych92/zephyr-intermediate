1291d2b789e6611ebb631c26fc8d864508e12da5
Akshay Chaturvedi <akshay.chaturvedi@hella.com>
2026-09-10 18:46:46 +0530

    l2 task1

--- app/src/main.c
+++ app/src/main.c
@@ -3,59 +3,56 @@
 
 LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
-#define STACK_SIZE 1024
+#define STACK_SIZE      1024
+#define PRIO            5
+#define INCREMENTS      1000000   /* each thread increments this many times */
 
-#define PRIO_LOW    7
-#define PRIO_MED    5
-#define PRIO_HIGH   3
-#define PRIO_COOP  -1
+/* Shared state - intentionally unprotected */
+static volatile uint32_t counter;
 
-void t_low_fun(void *p1, void *p2, void *p3)
+static struct k_sem done_sem;
+
+static K_MUTEX_DEFINE(counter_mtx);
+
+void worker_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        LOG_INF("T_LOW running");
-        k_msleep(300);
+    const char *name = k_thread_name_get(k_current_get());
+
+    for (int i = 0; i < INCREMENTS; i++) {
+
+        k_mutex_lock(&counter_mtx, K_FOREVER);
+        counter++;
+        k_mutex_unlock(&counter_mtx);
     }
+
+    LOG_INF("[%s] finished", name);
+    k_sem_give(&done_sem);
 }
 
-void t_med_fun(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_MED running");
-        k_msleep(200);
-    }
-}
-
-void t_high_fun(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_HIGH running");
-        k_msleep(100);
-    }
-}
-
-void t_coop_fun(void *p1, void *p2, void *p3)
-{
-    LOG_INF("[COOP] starting - will run 5 steps without yielding");
-    for(int loop=0; loop<5; loop++) {
-        LOG_INF("T_COOP running");
-    }
-    LOG_INF("T_COOP finished");
-    k_yield();
-
-}
-
-K_THREAD_DEFINE(thread_low, STACK_SIZE, t_low_fun,
-                NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(thread_med, STACK_SIZE, t_med_fun,
-                NULL, NULL, NULL, PRIO_MED, 0, 0);
-K_THREAD_DEFINE(thread_high, STACK_SIZE, t_high_fun,
-                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
-K_THREAD_DEFINE(thread_coop, STACK_SIZE, t_coop_fun,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(worker_a, STACK_SIZE, worker_fn, NULL, NULL, NULL,
+                PRIO, 0, 0);
+K_THREAD_DEFINE(worker_b, STACK_SIZE, worker_fn, NULL, NULL, NULL,
+                PRIO, 0, 0);
 
 int main(void)
 {
+    k_sem_init(&done_sem, 0, 2);
+
+    LOG_INF("=== L2 Demo 1: Shared Counter Corruption ===");
+    LOG_INF("Expected final value: %d", INCREMENTS * 2);
+
+    /* Wait for both workers to complete */
+    k_sem_take(&done_sem, K_FOREVER);
+    k_sem_take(&done_sem, K_FOREVER);
+
+    LOG_INF("Actual  final value: %u", counter);
+
+    if (counter == INCREMENTS * 2) {
+        LOG_WRN("No race this run - timing-dependent, try again");
+    } else {
+        LOG_ERR("Race condition confirmed: lost %d updates",
+                (INCREMENTS * 2) - counter);
+    }
+
     return 0;
 }
-
